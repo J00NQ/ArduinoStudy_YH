f535d5bf1116112f52b8e317589503771e3ad1f7
User <kjk020913@gmail.com>
2026-02-10 13:04:03 +0900

    조건문 오류 수정

--- Servo_Led/Servo_Led.ino
+++ Servo_Led/Servo_Led.ino
@@ -19,7 +19,7 @@ void loop() {
   int mVal = map(analogVal, 0, 1023, 0, 180);
   pos = mVal;
   myservo.write(pos);              
-  if (pos >= 30 || pos <= 150){     // 정상범위(30~150)이면 초록색 led 점등
+  if (pos >= 30 && pos <= 150){     // 정상범위(30~150)이면 초록색 led 점등
     digitalWrite(ledGreen, HIGH);
     digitalWrite(ledRed, LOW);
   } else {                          // 정상범위 벗어날 경우 빨간색 led 점등
